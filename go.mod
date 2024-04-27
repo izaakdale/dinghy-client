c3063a129d52616f71e6577eff4db28ba93856d4
Izaak Dale <izaakdaledev@gmail.com>
2024-04-27 16:48:00 -0700

    initial commit of dinghy client
    Signed-off-by: Izaak Dale <izaakdaledev@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/izaakdale/dinghy-client
 
 go 1.22.0
+
+require (
+	github.com/izaakdale/dinghy-agent v0.0.0-20240427225804-e3c72b166740
+	github.com/izaakdale/ittp v0.1.0
+	google.golang.org/grpc v1.56.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.11.0 // indirect
+	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,76 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 
+	v1 "github.com/izaakdale/dinghy-agent/api/v1"
+	"github.com/izaakdale/ittp"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
+)
+
+func main() {
+	conn, err := grpc.Dial(os.Getenv("DINGHY_AGENT_ADDR"), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		panic(err)
+	}
+	client := v1.NewAgentClient(conn)
+
+	mux := ittp.NewServeMux()
+
+	mux.Post("/", func(w http.ResponseWriter, r *http.Request) {
+		var req v1.InsertRequest
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		_, err := client.Insert(r.Context(), &req)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusAccepted)
+	})
+
+	mux.Get("/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := r.PathValue("key")
+		req := v1.FetchRequest{Key: key}
+
+		resp, err := client.Fetch(r.Context(), &req)
+		if err != nil {
+			if status.Code(err) == codes.NotFound {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		if err = json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	mux.Delete("/{key}", func(w http.ResponseWriter, r *http.Request) {
+		key := r.PathValue("key")
+		req := v1.DeleteRequest{Key: key}
+
+		_, err := client.Delete(r.Context(), &req)
+		if err != nil {
+			if status.Code(err) == codes.NotFound {
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+
+	http.ListenAndServe(fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT")), mux)
 }
